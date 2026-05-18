a6b8816d208679d8d6244cda6bc0405cd6c0a100
Fabricio <Fabricioriosssss@gmail.com>
2026-05-17 21:16:04 -0300

    Initial Commit

---
+++ Habito/HabitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitoApp.swift
+//  Habito
+//
+//  Created by Fabricio Exequiel Rios on 17/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
